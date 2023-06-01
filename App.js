24f72250ba204e98099ced4a10de83094e82eb7a
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-05-31 21:33:52 -0700

    Added goal list

--- App.js
+++ App.js
@@ -2,7 +2,14 @@ import { StyleSheet, Text, View, Button} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
+      <View>
+        <TextInput placeholder="Your course goal!" />
+        <Button title="Add Goal"/>
+      </View>
+      <View>
+        <Text>List of goals...</Text>
+      </View>
     </View>
   );
 }
