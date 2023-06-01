c629e21f75928ad55989cfce157431f96adf8dad
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-05-31 21:46:47 -0700

    styling

--- App.js
+++ App.js
@@ -17,14 +17,18 @@ export default function App() {
 const styles = StyleSheet.create({
   appContainer: {
     padding: 50,
+    paddingHorizontal: 16 
   },
   inputContainer: {
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    alignItems: 'center'
   },
   textInput: {
     borderWidth: 1,
     borderColor: '#cccccc',
-    width: '80%'
+    width: '70%',
+    marginRight: 8,
+    padding: 8
   }
 });
