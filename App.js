6fe8ae283b2c23fb59e84e1cc6948050ae3f9fd7
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-05-31 22:10:28 -0700

    added course list

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
 export default function App() {
 
   const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
 
   function goalInputHandler(enteredText) {
     setEnteredGoalText(enteredText);
@@ -11,6 +12,7 @@ export default function App() {
   
   function addGoalHandler() {
     console.log(enteredGoalText);
+    setCourseGoals(currentCourseGoals => [...currentCourseGoals, enteredGoalText]);
   };
 
   return (
@@ -27,7 +29,7 @@ export default function App() {
         <Button title="Add Goal" onPress={addGoalHandler}/>
       </View>
       <View style={styles.goalsContainer}>
-        <Text>List of goals...</Text>
+        {courseGoals.map( (goal) => { <Text>{goal}</Text> })}
       </View>
     </View>
   );
