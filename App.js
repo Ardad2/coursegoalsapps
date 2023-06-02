06e5ed4155ae9ab3035c2689a60a24c7c2c27b01
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-06-01 21:36:01 -0700

    reset text after entering

--- App.js
+++ App.js
@@ -2,41 +2,24 @@ import {useState} from 'react';
 import { 
   StyleSheet,  
   View, 
-  Button, 
-  TextInput,
   FlatList
 } from 'react-native';
 
 import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
 
-  const [enteredGoalText, setEnteredGoalText] = useState('');
   const [courseGoals, setCourseGoals] = useState([]);
-
-  function goalInputHandler(enteredText) {
-    setEnteredGoalText(enteredText);
-  };
   
-  function addGoalHandler() {
+  function addGoalHandler(enteredGoalText) {
     console.log(enteredGoalText);
     setCourseGoals(currentCourseGoals => [...currentCourseGoals, {text: enteredGoalText, id: Math.random().toString() }       ]);
   };
 
   return (
     <View style={styles.appContainer}>
-      <View style ={styles.inputContainer}>
-        <TextInput 
-        style={styles.textInput} 
-        placeholder="Your course goal!"
-        
-        //Note if it was goalInputHandler() it would get executed as soon as React starts.
-
-         onChangeText={goalInputHandler}
-         />
-        <Button title="Add Goal" onPress={addGoalHandler}/>
-        
-      </View>
+      <GoalInput onAddGoal={addGoalHandler}/>
       <View style={styles.goalsContainer}>
         <FlatList data={courseGoals} renderItem = {itemData => {
           return <GoalItem text={itemData.item.text}/>
@@ -55,22 +38,6 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 50,
     paddingHorizontal: 16 
-  },
-  inputContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: '#cccccc'
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: '#cccccc',
-    width: '70%',
-    marginRight: 8,
-    padding: 8
   },
   goalsContainer: {
     flex: 5
