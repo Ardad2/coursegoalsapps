a4e2530908d3d2d6f27c529e8531e83b8a83843d
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-06-01 21:45:25 -0700

    added delete functionality

--- App.js
+++ App.js
@@ -17,8 +17,10 @@ export default function App() {
     setCourseGoals(currentCourseGoals => [...currentCourseGoals, {text: enteredGoalText, id: Math.random().toString() }       ]);
   };
 
-  function deleteGoalHandler() {
-    console.log('Delete');
+  function deleteGoalHandler(id) {
+    setCourseGoals( (currentCourseGoals) => {
+      return currentCourseGoals.filter((goal) => goal.id != id);
+    } );
   }
 
   return (
@@ -26,7 +28,11 @@ export default function App() {
       <GoalInput onAddGoal={addGoalHandler}/>
       <View style={styles.goalsContainer}>
         <FlatList data={courseGoals} renderItem = {itemData => {
-          return <GoalItem text={itemData.item.text} onDeleteItem={deleteGoalHandler}/>
+          return <GoalItem 
+          text={itemData.item.text} 
+          id = {itemData.item.id}
+          onDeleteItem={deleteGoalHandler}
+          />
 
         }}
         keyExtractor={(item,index) => {return item.id}} 
